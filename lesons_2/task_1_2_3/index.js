122f1868b6cdd123cea2fd5fec3f5218b3fd9489
Andrii Pankovetskyi <pankovskiy@kasseler-cms.net>
2017-11-08 12:39:51 +0200

    task 1_2_3 function arguments;

---
+++ lesons_2/task_1_2_3/index.js
@@ -0,0 +1,30 @@
+//task_1
+function getArguments() {
+    return arguments.length;
+}
+
+console.log(getArguments(2, 3, 3, 4));
+
+//task_2
+function getStringArguments() {
+    var count = 0;
+    for (var i=0; i<arguments.length; i++) {
+        if (typeof arguments[i] === 'string') {
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(getStringArguments('3242','sdfsd',3,4,'0',6,7,8,9,'dfsfds'));
+
+//task_3
+function getSumArguments() {
+    var sum=0;
+    for (var i = 0; i < arguments.length; i++) {
+        sum+=arguments[i];
+    }
+    return sum;
+}
+
+console.log(getSumArguments(1,2,3,4,5,6,7));
